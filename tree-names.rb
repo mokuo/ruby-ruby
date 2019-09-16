befd6d47e9ac4810459423776cf1239a08555e4a
mokuo <tennis10988.yk@gmail.com>
2019-09-16 19:26:19 +0900

    3.6.3

--- tree-names.rb
+++ tree-names.rb
@@ -1,20 +1,15 @@
-def preorder(tree)
+def postorder(tree)
   if tree[0].start_with?("節")
-    preorder(tree[1])
-    preorder(tree[2])
-  elsif tree[0].start_with?("葉")
-    p(tree[0])
+    postorder(tree[1])
+    postorder(tree[2])
   end
+  p(tree[0])
 end
 
 node1 = [
   "節1",
-  [
-    "節2",
-    ["葉A"],
-    ["節3" ,["葉B"], ["葉C"]]
-  ],
-  ["葉D"]
+  ["節2",["葉A"],["葉B"]],
+  ["節3",["葉C"],["葉D"]]
 ]
 
-preorder(node1)
+postorder(node1)
