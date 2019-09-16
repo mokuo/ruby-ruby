b9c24dbd6452b400a189a83dd2f0096369291515
mokuo <tennis10988.yk@gmail.com>
2019-09-16 23:23:32 +0900

    5.5

--- interp.rb
+++ interp.rb
@@ -2,28 +2,32 @@
 
 require 'minruby'
 
-def evaluate(tree)
+def evaluate(tree, env)
   case tree[0]
   when 'lit'
     tree[1]
   when '+'
-    evaluate(tree[1]) + evaluate(tree[2])
+    evaluate(tree[1], env) + evaluate(tree[2], env)
   when '-'
-    evaluate(tree[1]) - evaluate(tree[2])
+    evaluate(tree[1], env) - evaluate(tree[2], env)
   when '*'
-    evaluate(tree[1]) * evaluate(tree[2])
+    evaluate(tree[1], env) * evaluate(tree[2], env)
   when '/'
-    evaluate(tree[1]) / evaluate(tree[2])
+    evaluate(tree[1], env) / evaluate(tree[2], env)
   when 'func_call' # 仮の実装
-    p(evaluate(tree[2]))
+    p(evaluate(tree[2], env))
   when 'stmts'
     i = 1
     last = nil
-    until tree[i].nil?
-      last = evaluate(tree[i])
+    while tree[i]
+      last = evaluate(tree[i], env)
       i += 1
     end
     last
+  when 'var_assign'
+    env[tree[1]] = evaluate(tree[2], env)
+  when 'var_ref'
+    env[tree[1]]
   end
 end
 
@@ -31,4 +35,5 @@ str = minruby_load
 
 tree = minruby_parse(str)
 
-evaluate(tree)
+env = {}
+evaluate(tree, env)
---
+++ test.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+x = 1
+y = x + 2
+p(y)
