d7457001203fcaa8fbc1b9a0143e7bcabf053014
mokuo <tennis10988.yk@gmail.com>
2019-09-21 21:39:19 +0900

    7.2

--- interp.rb
+++ interp.rb
@@ -2,53 +2,53 @@
 
 require 'minruby'
 
-def evaluate(tree, env)
+def evaluate(tree, genv, lenv)
   case tree[0]
   when 'lit'
     tree[1]
   when '+'
-    evaluate(tree[1], env) + evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) + evaluate(tree[2], genv, lenv)
   when '-'
-    evaluate(tree[1], env) - evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) - evaluate(tree[2], genv, lenv)
   when '*'
-    evaluate(tree[1], env) * evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) * evaluate(tree[2], genv, lenv)
   when '/'
-    evaluate(tree[1], env) / evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) / evaluate(tree[2], genv, lenv)
   when '%'
-    evaluate(tree[1], env) % evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) % evaluate(tree[2], genv, lenv)
   when '**'
-    evaluate(tree[1], env)**evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv)**evaluate(tree[2], genv, lenv)
   when '=='
-    evaluate(tree[1], env) == evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) == evaluate(tree[2], genv, lenv)
   when '<'
-    evaluate(tree[1], env) < evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) < evaluate(tree[2], genv, lenv)
   when '>'
-    evaluate(tree[1], env) > evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) > evaluate(tree[2], genv, lenv)
   when 'func_call' # 仮の実装
-    p(evaluate(tree[2], env))
+    p(evaluate(tree[2], genv, lenv))
   when 'stmts'
     i = 1
     last = nil
     while tree[i]
-      last = evaluate(tree[i], env)
+      last = evaluate(tree[i], genv, lenv)
       i += 1
     end
     last
   when 'var_assign'
-    env[tree[1]] = evaluate(tree[2], env)
+    env[tree[1]] = evaluate(tree[2], genv, lenv)
   when 'var_ref'
     env[tree[1]]
   when 'if'
-    if evaluate(tree[1], env)
-      evaluate(tree[2], env)
+    if evaluate(tree[1], genv, lenv)
+      evaluate(tree[2], genv, lenv)
     else
-      evaluate(tree[3], env)
+      evaluate(tree[3], genv, lenv)
     end
   when 'while'
-    evaluate(tree[2], env) while evaluate(tree[1], env)
+    evaluate(tree[2], genv, lenv) while evaluate(tree[1], genv, lenv)
   when 'while2'
-    evaluate(tree[2], env)
-    evaluate(tree[2], env) while evaluate(tree[1], env)
+    evaluate(tree[2], genv, lenv)
+    evaluate(tree[2], genv, lenv) while evaluate(tree[1], genv, lenv)
   end
 end
 
@@ -56,5 +56,5 @@ str = minruby_load
 
 tree = minruby_parse(str)
 
-env = {}
-evaluate(tree, env)
+lenv = {}
+evaluate(tree, genv, lenv)
