36619decb71cf9dac2747c22dfd2e650b840ccb9
mokuo <tennis10988.yk@gmail.com>
2019-09-23 17:09:33 +0900

    9.3.2

---
+++ fizzbuzz.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+i = 1
+while i <= 100
+  if i % 3 == 0
+    if i % 5 == 0
+      p('FizzBuzz')
+    else
+      p('Fizz')
+    end
+  else
+    if i % 5 == 0
+      p('Buzz')
+    else
+      p(i)
+    end
+  end
+  i = i + 1
+end
